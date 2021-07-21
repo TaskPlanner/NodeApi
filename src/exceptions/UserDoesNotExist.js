07880665885c15ac71d44327c5f3096e449b016a
Mateusz Malinowski <malina314@op.pl>
2021-07-21 11:41:14 +0200

    refactor exceptions

--- src/exceptions/UserDoesNotExist.js
+++
@@ -1,9 +0,0 @@
-const Exception = require("./Exception");
-
-class UserDoesNotExist extends Exception {
-  constructor() {
-    super(401, "This user does not exist");
-  }
-}
-
-module.exports = UserDoesNotExist;
